02bc25855f9b60ef269a8959802e8372d47f3880
加藤　優奈 <yuvernacrew@gmail.com>
2017-02-06 22:25:57 +0900

    topの改修

--- 2017/js/default.js
+++ 2017/js/default.js
@@ -2,4 +2,32 @@ $(function(){
 	$('#nav-toggle').click(function(){
 		$('.mobile-nav').toggleClass('open');
 	});
-})
+
+  $('.slick').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    autoplay:true,
+    autoplaySpeed:2000,
+    slidesToShow: 4,
+    dots: false,
+    arrows:false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
