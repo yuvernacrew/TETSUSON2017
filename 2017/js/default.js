774199f294047eef10b7925b0fbdd6cdd614a885
加藤　優奈 <yuvernacrew@gmail.com>
2017-02-10 23:53:55 +0900

    様々な変更

--- 2017/js/default.js
+++ 2017/js/default.js
@@ -1,6 +1,15 @@
 $(function(){
-  console.log('god');
 	$('#nav-toggle').click(function(){
 		$('.mobile-nav').toggleClass('open');
 	});
+
+	$('.staff-img').on({
+	'mouseenter':function(){
+		$(this).find('.staff-description').fadeIn()
+	},
+	'mouseleave':function(){
+		$(this).find('.staff-description').fadeOut()
+	}
+});
+
 });
